5fa5987113feb8e2d446789bd1bab463951392b8
jeferson <jeferson.barbosa@mercadolivre.com>
2022-03-29 11:23:44 -0300

    Added melhorar message

--- src/test/java/com/meli/obterdiploma/service/ObterDiplomaTest.java
+++ src/test/java/com/meli/obterdiploma/service/ObterDiplomaTest.java
@@ -3,13 +3,14 @@ package com.meli.obterdiploma.service;
 import com.meli.obterdiploma.model.StudentDTO;
 import com.meli.obterdiploma.model.SubjectDTO;
 import com.meli.obterdiploma.repository.IStudentDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,32 +24,48 @@ import java.util.List;
 public class ObterDiplomaTest {
 
     @Mock
-    IStudentDAO studentDAO;
+    IStudentDAO studentDAOMock;
 
     @InjectMocks
     private ObterDiplomaService obterDiplomaService;
 
+    private List<SubjectDTO> subjectDTOList;
+    private StudentDTO studentDTO;
+
     @Test
     public void SizeDAOTest() {
-        Mockito.when(studentDAO.count()).thenReturn(23);
-
+        Mockito.when(studentDAOMock.count()).thenReturn(23);
         String test = obterDiplomaService.serviceCount();
         assertEquals(test, "Count is: 23");
+    }
+
+    @BeforeEach
+    public void setCOnfig() {
+        subjectDTOList = new ArrayList<>();
+        subjectDTOList.add(new SubjectDTO("Matemática", 3.0));
+        subjectDTOList.add(new SubjectDTO("Português", 4.0));
+
+        studentDTO = new StudentDTO(3L,"Jimenez",null, 0.0, subjectDTOList);
     }
 
     @Test
     public void calculateAverageTest() {
 
-        assertNotNull(studentDAO);
-
-        List<SubjectDTO> subjectDTOList = new ArrayList<>();
-        subjectDTOList.add(new SubjectDTO("Matemática", 3.0));
-        subjectDTOList.add(new SubjectDTO("Português", 4.0));
-
-        Mockito.when(studentDAO.findById(3L)).thenReturn(new StudentDTO(3L,"Jimenez",null, 0.0, subjectDTOList));
-
+        assertNotNull(studentDAOMock);
+        Mockito.when(studentDAOMock.findById(3L)).thenReturn(studentDTO);
         StudentDTO studentDTO = obterDiplomaService.analyzeScores(3L);
 
         assertEquals(studentDTO.getAverageScore(),3.5);
+    }
+
+    @Test
+    @Tag("GrretingMessage")
+    public void podeMelhorarGreetingMessageTest() {
+
+        Mockito.when(studentDAOMock.findById(Mockito.anyLong())).thenReturn(studentDTO);
+        StudentDTO studentDTO = obterDiplomaService.analyzeScores(50L);
+
+        assertEquals(studentDTO.getMessage(), "O aluno Jimenez obteve uma média de 3,5. Você pode melhorar.");
+
     }
 }
